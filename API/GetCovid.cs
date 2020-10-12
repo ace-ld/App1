547226703173ffb55fddf42a71a7b24db3af23f2
ace-ld <54129812+ace-ld@users.noreply.github.com>
2020-10-12 15:21:35 +0300

    ---

---
+++ API/GetCovid.cs
@@ -0,0 +1,27 @@
+﻿using CovidApi;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace API
+{
+    public class GetCovid
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        // запрос к апи
+        public static async Task<Root> RequestApi()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var streamTask = client.GetStreamAsync("https://api.covid19api.com/summary");
+            var myDeserializedClass = await JsonSerializer.DeserializeAsync<Root>(await streamTask);
+
+            return myDeserializedClass;
+        }
+    }
+}
--- App1/MainPage.xaml.cs
+++ App1/MainPage.xaml.cs
@@ -1,9 +1,5 @@
-﻿using CovidApi;
+﻿using API;
 using System.Collections.Generic;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text.Json;
-using System.Threading.Tasks;
 using Windows.UI.Xaml.Controls;
 
 namespace App1
@@ -11,7 +7,6 @@ namespace App1
     public sealed partial class MainPage : Page
     {
         private readonly List<string> CountryList = new List<string>();
-        private static readonly HttpClient client = new HttpClient();
 
         public MainPage()
         {
@@ -19,22 +14,9 @@ namespace App1
             GetCountry();
         }
 
-        // запрос к апи
-        private async Task<Root> RequestApi()
-        {
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue("application/json"));
-
-            var streamTask = client.GetStreamAsync("https://api.covid19api.com/summary");
-            var myDeserializedClass = await JsonSerializer.DeserializeAsync<Root>(await streamTask);
-
-            return myDeserializedClass;
-        }
-
         private async void GetCountry()
         {
-            var repo = await RequestApi();
+            var repo = await GetCovid.RequestApi();
             foreach (var c in repo.Countries)
                 CountryList.Add(c.CountryStr);
         }
@@ -43,7 +25,7 @@ namespace App1
         {
             progressBar.IsIndeterminate = true;
 
-            var repo = await RequestApi();
+            var repo = await GetCovid.RequestApi();
             string countryName = e.AddedItems[0].ToString();
 
             foreach (var r in repo.Countries)
